6b572c6b96a44825438b4ade08b95926b328ff02
Viktoria <empty6022020@gmail.com>
2025-06-23 20:45:03 +0000

    Changes in the Menu. Section opens only when you click on the arrow.

--- data/static/js/menu.js
+++ data/static/js/menu.js
@@ -61,7 +61,7 @@ class Menu {
                     navigator.appendChild(div);
 
                     /** A header click handler for switching the visibility of nested sections */
-                    title.addEventListener('click', () => {
+                    arrowSvg.addEventListener('click', () => {
                         nestedDiv.classList.toggle('hidden');
                         arrowSvg.classList.toggle('up');
                     });
